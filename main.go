6497cd5301e5f7be7df667e13121b38ea67b3f18
jcourtney5 <jesse.courtney5@gmail.com>
2025-12-28 17:57:02 -0700

    added reading of file and printing 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const fileName = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Failed to open file %s, %v\n", fileName, err)
+	}
+	defer f.Close()
+
+	fmt.Printf("Reading data from %s\n", fileName)
+	fmt.Println("=====================================")
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := f.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("end of file")
+			}
+			fmt.Printf("Error occured while reading file %s\n", err.Error())
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
+
 }
