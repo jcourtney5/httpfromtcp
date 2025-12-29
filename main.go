1fc9bbc731d467a288b49d59a5ce8b8030e1ffeb
jcourtney5 <jesse.courtney5@gmail.com>
2025-12-28 18:27:37 -0700

    changed reading of file to be in a reusable function that uses a go routine and a channel

--- main.go
+++ main.go
@@ -12,46 +12,60 @@ import (
 const fileName = "messages.txt"
 
 func main() {
-
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("Failed to open file %s, %v\n", fileName, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", fileName)
 	fmt.Println("=====================================")
 
-	buffer := make([]byte, 8)
-	var lineBuilder strings.Builder
+	linesCh := getLinesChannel(f)
+	for line := range linesCh {
+		fmt.Println("read:", line)
+	}
+}
 
-	for {
-		n, err := f.Read(buffer)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		buffer := make([]byte, 8)
+		var lineBuilder strings.Builder
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("Error occured while reading file %s\n", err.Error())
 				break
 			}
-			fmt.Printf("Error occured while reading file %s\n", err.Error())
-			break
-		}
 
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		if len(parts) == 1 {
-			lineBuilder.WriteString(parts[0])
-		} else if len(parts) > 1 {
-			for index, part := range parts {
-				lineBuilder.WriteString(part)
-				if index != len(parts)-1 {
-					fmt.Printf("read: %s\n", lineBuilder.String())
-					lineBuilder.Reset()
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			if len(parts) == 1 {
+				lineBuilder.WriteString(parts[0])
+			} else if len(parts) > 1 {
+				for index, part := range parts {
+					lineBuilder.WriteString(part)
+					if index != len(parts)-1 {
+						lines <- lineBuilder.String()
+						lineBuilder.Reset()
+					}
 				}
 			}
 		}
-	}
 
-	if lineBuilder.Len() > 0 {
-		fmt.Printf("read: %s\n", lineBuilder.String())
-	}
+		if lineBuilder.Len() > 0 {
+			lines <- lineBuilder.String()
+			lineBuilder.Reset()
+		}
+	}()
 
+	return lines
 }
