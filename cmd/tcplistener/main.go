7f0e01d26394960ec2116668e623b4b8a33c843d
jcourtney5 <jesse.courtney5@gmail.com>
2025-12-31 22:02:19 -0700

    in tcplistener replaced getLinesChannel with new parsing functionality from RequestFromReader

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/jcourtney5/httpfromtcp/internal/request"
 )
 
 const port = ":42069"
@@ -27,55 +26,15 @@ func main() {
 		defer conn.Close()
 		fmt.Println("Accepted new connection")
 
-		linesCh := getLinesChannel(conn)
-		for line := range linesCh {
-			fmt.Println(line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("Failed to read request line %v\n", err)
 		}
+		fmt.Println("Request line:")
+		fmt.Println("- Method:", r.RequestLine.Method)
+		fmt.Println("- Target:", r.RequestLine.RequestTarget)
+		fmt.Println("- Version:", r.RequestLine.HttpVersion)
 
 		fmt.Println("connection has been closed")
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(lines)
-
-		buffer := make([]byte, 8)
-		var lineBuilder strings.Builder
-
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("Error occured while reading file %s\n", err.Error())
-				break
-			}
-
-			str := string(buffer[:n])
-			parts := strings.Split(str, "\n")
-			if len(parts) == 1 {
-				lineBuilder.WriteString(parts[0])
-			} else if len(parts) > 1 {
-				for index, part := range parts {
-					lineBuilder.WriteString(part)
-					if index != len(parts)-1 {
-						lines <- lineBuilder.String()
-						lineBuilder.Reset()
-					}
-				}
-			}
-		}
-
-		if lineBuilder.Len() > 0 {
-			lines <- lineBuilder.String()
-			lineBuilder.Reset()
-		}
-	}()
-
-	return lines
 }
